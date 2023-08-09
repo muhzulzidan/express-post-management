b480834290bd6ccf4194c788cd4842163c03fd53
muhzulzidan <mzulzidan@gmail.com>
2023-08-09 21:14:47 +0800

    update crud

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -21,4 +21,81 @@ const createPost = async (req, res) => {
     }
 };
 
-module.exports = { createPost };
+const getAllPosts = async (req, res) => {
+    try {
+        const allPosts = await postNew.findAll();
+        return res.status(200).send(allPosts);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ message: 'Error occurred', error });
+    }
+};
+
+const getPost = async (req, res) => {
+    try {
+        const { postID } = req.params;
+        const post = await postNew.findOne({ where: { id: postID } });
+
+        if (!post) {
+            return res.status(404).send({ message: 'Post not found.' });
+        }
+
+        return res.status(200).send(post);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ message: 'Error occurred', error });
+    }
+};
+
+
+const updatePost = async (req, res) => {
+    try {
+        const { id: user_id } = req.user;
+        const { postID } = req.params;
+        const { title, body } = req.body;
+
+        if (!user_id || !title || !body) {
+            return res.status(400).send({ message: 'All fields are required.' });
+        }
+
+        const updatedPost = await postNew.update(
+            { title, body },
+            { where: { id: postID, user_id } }
+        );
+
+        if (updatedPost[0] === 0) {
+            return res.status(404).send({ message: 'Post not found or unauthorized.' });
+        }
+
+        return res.status(200).send({ message: 'Post updated successfully.' });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ message: 'Error occurred', error });
+    }
+};
+
+const deletePost = async (req, res) => {
+    try {
+        const { id: user_id } = req.user;
+        const { postID } = req.params;
+
+        const deletedRowsCount = await postNew.destroy({
+            where: { id: postID, user_id }
+        });
+
+        if (deletedRowsCount === 0) {
+            return res.status(404).send({ message: 'Post not found or unauthorized.' });
+        }
+
+        return res.status(200).send({ message: 'Post deleted successfully.' });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ message: 'Error occurred', error });
+    }
+};
+
+
+
+module.exports = { createPost, getAllPosts, getPost, updatePost, deletePost };
+
+
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,9 +1,21 @@
 // post.route.js
 const express = require('express');
 const router = express.Router();
-const { createPost } = require('../controllers/post.controller');
 const { verifyToken } = require('../middlewares/verifyToken');
+
+const { getAllPosts, getPost, createPost, updatePost, deletePost } = require('../controllers/post.controller');
+
+
+// Get a specific post
+router.get('/:postID', getPost);
+// Get all posts
+router.get('/', getAllPosts);
 // Create a new post
 router.post('/', verifyToken, createPost);
+// Update a post
+router.put('/:postID', verifyToken, updatePost);
+// Delete a post
+router.delete('/:postID', verifyToken, deletePost);
+
 
 module.exports = router;
